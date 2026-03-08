6d5ea91c2ba921a6fce04e7287671e42f1f20006
Wild-Mane <fizlevine@gmail.com>
2026-03-08 00:12:21 -0800

    Implemenasi kalkulator penjumlahan

--- TP_MODUL3_103022400063/Form1.Designer.cs
+++ TP_MODUL3_103022400063/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button13 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -62,6 +63,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "8";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -71,6 +73,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "7";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -80,6 +83,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "6";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -89,6 +93,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -98,6 +103,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -107,6 +113,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "3";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -126,6 +133,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -135,6 +143,7 @@
             this.button10.TabIndex = 9;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -144,6 +153,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "=";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -153,6 +163,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "+";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // textBox1
             // 
@@ -162,12 +173,24 @@
             this.textBox1.TabIndex = 12;
             this.textBox1.Text = "Label Output";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(529, 238);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(99, 188);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "C";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button13);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
@@ -182,7 +205,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "go";
+            this.Text = "Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -205,6 +228,7 @@
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button13;
     }
 }
 
--- TP_MODUL3_103022400063/Form1.cs
+++ TP_MODUL3_103022400063/Form1.cs
@@ -12,6 +12,12 @@ namespace TP_MODUL3_103022400063
 {
     public partial class Form1 : Form
     {
+        String input = "";
+        double hasil = 0;
+        String operasi = "";
+        bool hasiloperasi = false;
+        string tester;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +30,131 @@ namespace TP_MODUL3_103022400063
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            input = "";
+            hasil = 0;
+            operasi = "";
+            hasiloperasi = false;
+            textBox1.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            input += button.Text;
+            textBox1.Text = input;
+        }
+
+        private void Evaluasi()
+        {
+            if (hasiloperasi)
+            {
+                double secondNumber = double.Parse(input);
+                switch (operasi)
+                {
+                    case "+":
+                        hasil += secondNumber;
+                        break;
+                }
+            }
+            textBox1.Text = hasil.ToString();
+            input = "";
+            hasiloperasi = false;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+                if (hasiloperasi)
+            {
+                Evaluasi();
+            }
+            Button button = (Button)sender;
+            operasi = button.Text;
+            hasil = double.Parse(input);
+            input = "";
+            hasiloperasi = true;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (hasiloperasi)
+            {
+                Evaluasi();
+            }
+            Button button = (Button)sender;
+            operasi = button.Text;
+            hasil = double.Parse(input);
+            input = "";
+            hasiloperasi = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
+
+
 }
